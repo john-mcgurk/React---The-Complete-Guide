7957aa97540a6a6923d93c54dbaa165e70842395
john-mcgurk <73965127+john-mcgurk@users.noreply.github.com>
2022-11-14 22:19:55 +0000

    Overlay for Cart

--- 5-food-order-webapp/src/App.js
+++ 5-food-order-webapp/src/App.js
@@ -1,13 +1,28 @@
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals"
+import { useState } from "react";
+import Cart from "./components/Cart/Cart";
+import CartProvider from './store/CartProvider';
+
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  }
+
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  }
+
   return (
-    <div>
-      <Header />
+    <CartProvider>
+      {cartIsShown && <Cart onClose={hideCartHandler}/>}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
-    </div>
+    </CartProvider>
   );
 }
 

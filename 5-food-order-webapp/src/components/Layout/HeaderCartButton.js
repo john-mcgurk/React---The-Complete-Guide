42258509b7ec7b188c2cf3941ac691af6686c987
john-mcgurk <73965127+john-mcgurk@users.noreply.github.com>
2022-11-15 00:27:03 +0000

    Using context to create interactive data in web app

--- 5-food-order-webapp/src/components/Layout/HeaderCartButton.js
+++ 5-food-order-webapp/src/components/Layout/HeaderCartButton.js
@@ -1,16 +1,43 @@
-import classes from './HeaderCartButton.module.css';
-import CartIcon from '../Cart/CartIcon';
-const HeaderCartButton = props => {
+import classes from "./HeaderCartButton.module.css";
+import CartIcon from "../Cart/CartIcon";
+import { useContext, useEffect, useState } from "react";
+import CartContext from "../../store/cart-context";
 
-    return <button className={classes.button}>
-        <span className={classes.icon}>
-            <CartIcon />
-        </span>
-        <span>Your Cart</span>
-        <span className={classes.badge}>
-            3
-        </span>
+const HeaderCartButton = (props) => {
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
+  const cartCtx = useContext(CartContext);
+  const {items} = cartCtx;
+
+  const numberOfCartItems = items.reduce((currentNumber, item) => {
+    return currentNumber + item.amount;
+  }, 0);
+
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnIsHighlighted(true);
+
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    }
+  }, [items]);
+
+  const btnClasses = `${classes.button} ${btnIsHighlighted ? classes.bump : ''}`;
+  return (
+    <button className={btnClasses} onClick={props.onClick}>
+      <span className={classes.icon}>
+        <CartIcon />
+      </span>
+      <span>Your Cart</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
+  );
 };
 
 export default HeaderCartButton;
--- 5-food-order-webapp/src/components/Meals/MealItem/MealItemForm.js
+++ 5-food-order-webapp/src/components/Meals/MealItem/MealItemForm.js
@@ -1,16 +1,37 @@
 import classes from './MealItemForm.module.css';
 import Input from '../../UI/Input';
+import { useRef, useState } from 'react';
+
 const MealItemForm = props => {
-    return <form className={classes.form}>
-        <Input label="Amount" input={{
+    const amountInputRef = useRef();
+    const [amountIsValid, setAmountisValid] = useState(true);
+
+    const submitHandler = event => {
+        event.preventDefault();
+        const enteredAmount = amountInputRef.current.value;
+        const enteredAmountNum = +enteredAmount;
+
+        if (enteredAmount.trim().length === 0 || enteredAmountNum < 1 || enteredAmountNum > 5) {
+            setAmountisValid(false);
+            return;
+        }
+        setAmountisValid(true);
+        props.onAddToCart(enteredAmountNum);
+    }
+    return <form className={classes.form} onSubmit={submitHandler}>
+        <Input
+            ref={amountInputRef}
+            label="Amount" 
+            input={{
             id: 'amount_' + props.id,
             type: 'number',
             min: '1',
             max: '5',
             step: '1',
-            default: '1'
+            defaultValue: '1',
         }} />
         <button>+ Add</button>
+        { !amountIsValid && <p>Please enter a valid amount (1-5).</p>}
     </form>
 };
 

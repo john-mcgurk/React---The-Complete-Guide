31171e54d22ebd0b0504502b12aa176189fe4b5c
john-mcgurk <73965127+john-mcgurk@users.noreply.github.com>
2022-11-29 22:23:05 +0000

    React Custom Hooks Tutorial
    Creating custom hooks - useHttp, which allows for app-wide logic on http requests whilst also executing variable requests, and transforming returned data.

--- 9-react-custom-hooks/src/App.js
+++ 9-react-custom-hooks/src/App.js
@@ -1,12 +1,44 @@
-import React from 'react';
-import BackwardCounter from './components/BackwardCounter';
-import ForwardCounter from './components/ForwardCounter';
+import React, { useEffect, useState, useCallback } from "react";
+
+import Tasks from "./components/Tasks/Tasks";
+import NewTask from "./components/NewTask/NewTask";
+import useHttp from "./hooks/use-http";
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+
+  const transformTasks = useCallback((tasks) => {
+    const loadedTasks = [];
+
+    for (const taskKey in tasks) {
+      loadedTasks.push({ id: taskKey, text: tasks[taskKey].text });
+    }
+
+    setTasks(loadedTasks);
+  }, []);
+
+  const { isLoading, error, sendRequest: fetchTasks } = useHttp();
+
+  useEffect(() => {
+    fetchTasks({
+      url: "https://react-http-1f7de-default-rtdb.firebaseio.com/tasks.json",
+    },
+    transformTasks);
+  }, [fetchTasks, transformTasks]);
+
+  const taskAddHandler = (task) => {
+    setTasks((prevTasks) => prevTasks.concat(task));
+  };
+
   return (
     <React.Fragment>
-      <ForwardCounter />
-      <BackwardCounter />
+      <NewTask onAddTask={taskAddHandler} />
+      <Tasks
+        items={tasks}
+        loading={isLoading}
+        error={error}
+        onFetch={fetchTasks}
+      />
     </React.Fragment>
   );
 }

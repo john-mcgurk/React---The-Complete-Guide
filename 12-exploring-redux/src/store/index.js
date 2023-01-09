90370a5dcb215d2eeac5aba67a2059dc8a632327
john-mcgurk <73965127+john-mcgurk@users.noreply.github.com>
2023-01-09 23:17:20 +0000

    Redux Authentication App
    Basic authentication app using redux tookit covering following topics
    
    - Organising multiple state slices
    - Separating context code into multiple files
    - Configuring stores to use multiple reducer functions with qualifiers.

--- 12-exploring-redux/src/store/index.js
+++ 12-exploring-redux/src/store/index.js
@@ -1,30 +1,11 @@
-import { configureStore, createSlice } from '@reduxjs/toolkit';
-
-const initialState = { counter: 0, showCounter: true };
-
-const counterSlice = createSlice({
-  name: 'counter',
-  initialState,
-  reducers: {
-    increment(state) {
-      state.counter++;
-    },
-    decrement(state) {
-      state.counter--;
-    },
-    increase(state, action) {
-      state.counter = state.count + action.payload;
-    },
-    toggleCounter(state) {
-      state.showCounter = !state.showCounter;
-    }
-  }
-});
+import { configureStore } from '@reduxjs/toolkit';
+import counterReducer from './counter';
+import authReducer from './auth';
 
 const store = configureStore({
-  reducer: counterSlice.reducer
-});
-
-export const counterActions = counterSlice.actions;
+  reducer: {
+    counter: counterReducer,
+    auth: authReducer
+}});
 
 export default store;

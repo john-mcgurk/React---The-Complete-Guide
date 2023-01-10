601f0bfcc8138f91939f6d0795f6e64c73f55e94
john-mcgurk <73965127+john-mcgurk@users.noreply.github.com>
2023-01-10 23:07:15 +0000

    Bookstore v2
    - Add API to store cart information as user adds/removes items
    - Add notification component to handle API errors.

--- 13-advanced-redux/src/store/ui-slice.js
+++ 13-advanced-redux/src/store/ui-slice.js
@@ -1,19 +1,25 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
 const uiSlice = createSlice({
-    name: 'ui',
-    initialState: {
-        cartIsVisible: false
+  name: "ui",
+  initialState: {
+    cartIsVisible: false,
+    notification: null,
+  },
+  reducers: {
+    toggle(state) {
+      state.cartIsVisible = !state.cartIsVisible;
     },
-    reducers: {
-        toggle(state) {
-            state.cartIsVisible = !state.cartIsVisible;
-        }
-    }
+    showNotification(state, action) {
+      state.notification = {
+        statusKey: action.payload.status,
+        title: action.payload.title,
+        message: action.payload.message,
+      };
+    },
+  },
 });
 
 export const uiActions = uiSlice.actions;
 
 export default uiSlice;
-
-

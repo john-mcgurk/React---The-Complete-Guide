50bca0060cf5d0884a31ca97e5f3f6a33d5def63
john-mcgurk <73965127+john-mcgurk@users.noreply.github.com>
2023-01-10 23:41:16 +0000

    Bookstore v3
    - Moved cart actions to separate file
    - Added HTTP Get and Put requests linked to firebase to allow realtime updating and persistence of cart using redux.

--- 13-advanced-redux/src/App.js
+++ 13-advanced-redux/src/App.js
@@ -4,8 +4,7 @@ import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
 import Notification from "./components/UI/Notification";
-import { uiActions } from "./store/ui-slice";
-
+import { fetchCartData, sendCartData } from "./store/cart-actions";
 let isInitial = true;
 
 function App() {
@@ -16,48 +15,18 @@ function App() {
   const notification = useSelector((state) => state.ui.notification);
 
   useEffect(() => {
+    dispatch(fetchCartData());
+  }, [dispatch]);
+
+  useEffect(() => {
     if (isInitial) {
       isInitial = false;
       return;
     }
-    dispatch(
-      uiActions.showNotification({
-        status: "pending",
-        title: "Sending...",
-        message: "Sending cart data!",
-      })
-    );
-    const sendCardData = async () => {
-      const response = await fetch(
-        "https://react-http-1f7de-default-rtdb.firebaseio.com/cart.json",
-        {
-          method: "PUT",
-          body: JSON.stringify(cart),
-        }
-      );
 
-      if (!response.ok) {
-        throw new Error("Sending cart data failed.");
-      }
-
-      dispatch(
-        uiActions.showNotification({
-          status: "success",
-          title: "Success",
-          message: "Sent cart data successfully!",
-        })
-      );
-    };
-
-    sendCardData().catch((error) => {
-      dispatch(
-        uiActions.showNotification({
-          status: "error",
-          title: "Error",
-          message: "Sending cart failed!",
-        })
-      );
-    });
+    if (cart.changed) {
+      dispatch(sendCartData(cart));
+    }
   }, [cart, dispatch]);
   return (
     <Fragment>

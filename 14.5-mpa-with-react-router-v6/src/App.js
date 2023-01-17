a5967252a14239348af98a42d3b15f5374a489a9
john-mcgurk <73965127+john-mcgurk@users.noreply.github.com>
2023-01-17 18:10:34 +0000

    useParams, dynamic routing and relative paths overview in React Router V6

--- 14.5-mpa-with-react-router-v6/src/App.js
+++ 14.5-mpa-with-react-router-v6/src/App.js
@@ -4,6 +4,7 @@ import {
 } from "react-router-dom";
 import HomePage from "./pages/Home";
 import Products from "./pages/Products";
+import ProductDetailPage from "./pages/ProductDetail";
 import RootLayout from "./pages/Root";
 import ErrorPage from "./pages/Error";
 
@@ -20,8 +21,9 @@ const router = createBrowserRouter([
     element: <RootLayout />,
     errorElement: <ErrorPage />,
     children: [
-      { path: "/", element: <HomePage /> },
+      { index: true, element: <HomePage /> },
       { path: "/products", element: <Products /> },
+      { path: "/products/:productId", element: <ProductDetailPage /> },
     ],
   },
 ]);

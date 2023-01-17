346596413ebfbdd4dd97dd16cb7deccc76e6a206
john-mcgurk <73965127+john-mcgurk@users.noreply.github.com>
2023-01-17 17:43:51 +0000

    Overview of React Router v6 core principles

---
+++ 14.5-mpa-with-react-router-v6/src/App.js
@@ -0,0 +1,35 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import HomePage from "./pages/Home";
+import Products from "./pages/Products";
+import RootLayout from "./pages/Root";
+import ErrorPage from "./pages/Error";
+
+// const routeDefinitions = createRoutesFromElements(
+//   <Route>
+//     <Route path="/" element={<HomePage />}></Route>
+//     <Route path="/products" element={<Products />}></Route>
+//   </Route>
+// )
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      { path: "/", element: <HomePage /> },
+      { path: "/products", element: <Products /> },
+    ],
+  },
+]);
+
+// const router = createBrowserRouter(routeDefinitions);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
---
+++ 14.5-mpa-with-react-router-v6/src/pages/Home.js
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+function HomePage() {
+  return (
+    <>
+      <h1>My Home Page</h1>
+      <p>Go to <Link to="/products">Products</Link></p>
+    </>
+  );
+}
+
+export default HomePage;

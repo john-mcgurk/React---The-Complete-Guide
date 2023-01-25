6ab1376ff1385ab95bc786225a0e8477f1d0bcf4
john-mcgurk <73965127+john-mcgurk@users.noreply.github.com>
2023-01-25 14:33:06 +0000

    Events App v3
    - Introduce useActionData and useNavigation hook
    - Reusable actions with dynamic parameters for post/patch request to api backend.

--- 14.5-mpa-with-react-router-v6/react-frontend/src/pages/EditEvent.js
+++ 14.5-mpa-with-react-router-v6/react-frontend/src/pages/EditEvent.js
@@ -2,7 +2,7 @@ import EventForm from '../components/EventForm';
 import { useRouteLoaderData } from 'react-router';
 function EditEventPage() {
     const data = useRouteLoaderData('event-detail');
-    return <EventForm event={data.event}/>;
+    return <EventForm event={data.event} method='patch'/>;
 }
 
 export default EditEventPage;

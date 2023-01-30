2d7385b5ac29e3f8afab88e248e10590dac60f4b
john-mcgurk <73965127+john-mcgurk@users.noreply.github.com>
2023-01-30 23:16:56 +0000

    Exploring basic dynamic routing and linking to pages in next js

--- 17-using-next-js/pages/news/index.js
+++ 17-using-next-js/pages/news/index.js
@@ -1,5 +1,19 @@
+import Link from "next/link";
+
 function NewsPage() {
-    return <h1>The News Page</h1>
+  return (
+    <>
+      <h1>The News Page</h1>
+      <ul>
+        <li>
+          <Link href="/news/nextjs-is-great">NextJS is Great!</Link>
+        </li>
+        <li>
+          <Link href="/news/something-else">Something else</Link>
+        </li>
+      </ul>
+    </>
+  );
 }
 
 export default NewsPage;
